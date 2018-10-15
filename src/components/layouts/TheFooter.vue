4cf943cd58710a7b9b4a2219c83508715c9a9970
Hao-Chang <hao.chang@contin-global.com>
2018-10-15 18:10:30 +0800

    add_header_and_footer_components

---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,65 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="row footer-top">
+                <p class="padding-top-xsm">{{ description }}</p>
+                <a v-for="item in contacts" href="#" v-title="item.title" :style="contactStyle">
+                    <i :class="`fa fa-${item.icon}`"></i>
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    import title from '@/directives/title'
+
+    export default {
+        name: 'TheFooter',
+        directives: {
+            title
+        },
+        data() {
+            return {
+                description: 'Vue.js',
+                contacts: [
+                    {
+                        icon: 'android',
+                        title: 'android',
+                    },
+                    {
+                        icon: 'apple',
+                        title: 'apple',
+                    },
+                    {
+                        icon: 'bath',
+                        title: 'bath'
+                    },
+                    {
+                        icon: 'bandcamp',
+                        title: 'bandcamp'
+                    }
+                ],
+                contactStyle: {
+                    paddingRight: '20px'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .footer
+    {
+        padding-top: 0px;
+        background: #4fc08d;
+        bottom: 0;
+        height: 50px;
+        color: #ffffff;
+    }
+    a:hover, a:focus
+    {
+        color: #000000;
+        transition: color .15s;
+    }
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="navbar navbar-default topnav">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" @click="toggleNav">
+                    <span class="sr-only">Toggle</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a href="/" class="navbar-brand">
+                    <span class="title">{{ logo.title}}</span>
+                    <img :src="require(`@/assets/${logo.src}`)" :alt="logo.title">
+                </a>
+            </div>
+            <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in:showCollapsedNav }]">
+                <ul class="nav navbar-nav">
+                    <li v-for="(item, index ) in navList" :class="{ active: index === activeNavIndex}">
+                        <a href="#" @click="changeNavIndex(index)">{{ item  }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+        data() {
+            return {
+                logo: {
+                    src: 'logo.png',
+                    title: 'vue.js'
+                },
+                navList: ['navBar1', 'navBar2', 'navBar3', 'navBar4'],
+                activeNavIndex: 0,
+                showCollapsedNav: false
+            }
+        },
+        methods: {
+            changeNavIndex(index) {
+                this.activeNavIndex = index
+            },
+            toggleNav() {
+               this.showCollapsedNav = !this.showCollapsedNav
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .title
+    {
+        display: none;
+    }
+    .navbar-default .navbar-nav > .active > a
+    {
+        background: #4fc08d;
+        color: #ffffff;
+    }
+</style>
